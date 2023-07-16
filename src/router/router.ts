b7a66f431c40216d657ef8ca547fb0b63ac40f3d
zetsuboii <hmzkrb@gmail.com>
2023-07-16 21:55:22 +0300

    feat: extend router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,40 +1,88 @@
-import { routeRequest, formatRouterCall } from "../utils";
-import { RouteConfig, RouteResponse,RouterError, Token, CairoSwap, Transaction, TransactionConfig } from "../types";
+import { buildRouteUrl, buildSwapCalldata } from "../utils";
+import {
+    RouteOptions,
+    RouteResponse,
+    Token,
+    Transaction,
+    TransactionConfig,
+    ProtocolId,
+} from "../types";
 
 export class Router {
-    private readonly url: string;
+    readonly DEFAULT_API_URL = "https://api.fibrous.finance";
+    readonly ROUTER_ADDRESS =
+        "0x1b23ed400b210766111ba5b1e63e33922c6ba0c45e6ad56ce112e5f4c578e62";
 
-    constructor(url: string) {
-        this.url = url;
+    private readonly apiUrl: string;
+    private readonly apiKey: string | null;
+
+    constructor(dedicatedUrl?: string, apiKey?: string) {
+        // Trim / at the end
+        if (dedicatedUrl && dedicatedUrl.endsWith("/"))
+            dedicatedUrl = dedicatedUrl.substring(0, dedicatedUrl.length - 1);
+        this.apiUrl = dedicatedUrl ?? this.DEFAULT_API_URL;
+        this.apiKey = apiKey ?? null;
     }
 
-    async route(options: RouteConfig): Promise<RouteResponse | RouterError> {
-        const res = await routeRequest<RouteResponse>(this.url, options);
-        return res
+    /**
+     * Gets the best route from the API
+     * @param options: Options for the route
+     * @throws
+     */
+    async getBestRoute(options: RouteOptions): Promise<RouteResponse> {
+        const excludeProtocols = (options.excludeProtocols ?? []).join(",");
+        const routeParams = Object.assign(options, { excludeProtocols });
+        const routeUrl = buildRouteUrl(`${this.apiUrl}/route`, routeParams);
+        return await fetch(routeUrl, {
+            headers: { "X-API-Key": this.apiKey },
+        }).then((response) => response.json());
     }
 
-    async tokenList(): Promise<Token[]> {
-        const res = await fetch(this.url + "/tokens");
-        if (!res.ok) {
-            throw new Error(res.statusText);
-        } else {
-            return res.json();
-        }
-    }
+    /**
+     * @returns Supported token list
+     */
+    async supportedTokens(): Promise<Record<string, Token>> {
+        const tokens: Token[] = await fetch(`${this.apiUrl}/tokens`, {
+            headers: { "X-API-Key": this.apiKey },
+        }).then((response) => response.json());
 
-    buildTransaction(config: TransactionConfig): Transaction {
-        const setInput: CairoSwap[] = formatRouterCall(
-            config.route,
-            config.slippage,
-            config.accountAddress
+        // Create a record of tokens by symbol
+        return tokens.reduce(
+            (acc, token) => Object.assign(acc, { [token.symbol]: token }),
+            {}
         );
-        const transaction: Transaction = {
-            contractAddress: config.routerAddress,
-            entryPoint: "swap",
-            call_data: setInput
-        }
-        return transaction
     }
 
+    /**
+     * @returns Supported protocol list
+     */
+    async supportedProtocols(): Promise<Record<string, ProtocolId>> {
+        const protocols: string[] = await fetch(`${this.apiUrl}/protocols`, {
+            headers: { "X-API-Key": this.apiKey },
+        }).then((response) => response.json());
+
+        // Create a record of protocols, starting from 1
+        return protocols.reduce(
+            (acc, protocol, idx) =>
+                Object.assign(acc, { [protocol]: (idx + 1) as ProtocolId }),
+            {}
+        );
+    }
+
+    /**
+     * Builds a Starknet transaction out of the route response
+     * @param config: Transaction configuration
+     * @returns Starknet transaction
+     */
+    buildTransaction(config: TransactionConfig): Transaction {
+        return {
+            contractAddress: this.ROUTER_ADDRESS,
+            entryPoint: "swap",
+            call_data: buildSwapCalldata(
+                config.route,
+                config.slippage,
+                config.accountAddress
+            ),
+        };
+    }
 }
- 
